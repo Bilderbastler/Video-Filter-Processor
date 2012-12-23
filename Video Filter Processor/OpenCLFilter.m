4126d27ff0f87881bb1b4c292e9687718ba74ac6
Florian Neumeister <fn@dtpr.de>
2012-12-23 15:29:48 +0100

    Working openCL Filter

--- Video Filter Processor/OpenCLFilter.m
+++ Video Filter Processor/OpenCLFilter.m
@@ -30,6 +30,8 @@
             gpu = cpu;
         }
         
+        //gpu = cpu;
+        
         cl_bool imageSupport = CL_FALSE;
         clGetDeviceInfo(gpu, CL_DEVICE_IMAGE_SUPPORT, sizeof(cl_bool), &imageSupport, NULL);
         if (imageSupport != CL_TRUE) {
@@ -46,7 +48,7 @@
         }
         
         // Bildformat:
-        format.image_channel_order = CL_ARGB;
+        format.image_channel_order = CL_RGBA;
         format.image_channel_data_type = CL_UNSIGNED_INT8;
         
         // Programm erzeugen
@@ -62,12 +64,32 @@
             NSLog(@"CL program could not be build from binary: %i", err);
         }
         
-        sampler = clCreateSampler(context, CL_FALSE, CL_ADDRESS_REPEAT, CL_FILTER_NEAREST, &err);
+        sampler = clCreateSampler(context, CL_FALSE, CL_ADDRESS_CLAMP, CL_FILTER_NEAREST, &err);
         if(err != CL_SUCCESS){
             NSLog(@"Could not create sampler");
         }
         
+        size_t itemSizes[3];
+        clGetDeviceInfo(gpu, CL_DEVICE_MAX_WORK_ITEM_SIZES, sizeof(size_t)*3, &itemSizes, NULL);
+
+        size_t groupSize;
+        clGetDeviceInfo(gpu, CL_DEVICE_MAX_WORK_GROUP_SIZE, sizeof(size_t), &groupSize, NULL);
+        
+        size_t itemDimension;
+        clGetDeviceInfo(gpu, CL_DEVICE_MAX_WORK_ITEM_DIMENSIONS, sizeof(size_t), &itemDimension, NULL);
+        
+        char buf[128];
+        clGetDeviceInfo(gpu, CL_DEVICE_NAME, 128, buf, NULL);
+        fprintf(stdout, "Device %s supports \n", buf);
+        clGetDeviceInfo(gpu, CL_DEVICE_VERSION, 128, buf, NULL);
+        fprintf(stdout, "%s\n", buf);
+        clGetDeviceInfo(cpu, CL_DEVICE_NAME, 128, buf, NULL);
+        fprintf(stdout, "Device %s supports \n", buf);
+        clGetDeviceInfo(cpu, CL_DEVICE_VERSION, 128, buf, NULL);
+        fprintf(stdout, "%s\n", buf);
+        
         clFinish(cmd_queue);
+        NSLog(@"fertig");
     }
     return self;
 }
@@ -76,13 +98,11 @@
     cl_mem inputData;
     cl_mem outputData;
     
+    size_t globalWorkSize = bufferWidth * bufferHeight;
+    
     cl_float4 shadows = {1.0f, self.blacks.r, self.blacks.g, self.blacks.b};
     cl_float4 gamma = {1.0f, self.mids.r, self.mids.g, self.mids.b};
     cl_float4 lights = {1.0f, self.highlights.r, self.highlights.g, self.highlights.b};
-    
-    size_t globalWorkSize = bufferWidth * bufferHeight;
-    
-    
 
     if (self.useImage) {
         // Kernel erzeugen
@@ -90,7 +110,6 @@
         if (err != CL_SUCCESS) {
             NSLog(@"CL image kernel could not be created: %i", err);
         }
-       
        
         inputData = clCreateImage2D(context, CL_MEM_READ_ONLY, &format, bufferWidth, bufferHeight, 0, NULL, &err);
         if (err != CL_SUCCESS) {
@@ -102,28 +121,31 @@
         }
         
         const size_t origin[3] = {0,0,0};
-        const size_t region[3] = {bufferWidth, bufferHeight, 0};
-        clEnqueueReadImage(cmd_queue, inputData, CL_TRUE, origin, region, bytesPerRow, 0, _buffer, 0, NULL, NULL);
-        
+        const size_t region[3] = {bufferWidth, bufferHeight, 1};
+        err = clEnqueueWriteImage(cmd_queue, inputData, CL_FALSE, origin, region, bytesPerRow, 0, base, 0, NULL, NULL);
+        if (err != CL_SUCCESS) {
+            NSLog(@"Could not upload input data: %i", err);
+        }
         
         // enqueue arguments
         clSetKernelArg(imageKernel, 0, sizeof(cl_mem), &inputData);
         clSetKernelArg(imageKernel, 1, sizeof(cl_mem), &outputData);
-        clSetKernelArg(imageKernel, 2, sizeof(cl_double3), &shadows);
-        clSetKernelArg(imageKernel, 3, sizeof(cl_double3), &gamma);
-        clSetKernelArg(imageKernel, 4, sizeof(cl_double3), &lights);
-        
-
+        clSetKernelArg(imageKernel, 2, sizeof(cl_sampler), &sampler);
+        clSetKernelArg(imageKernel, 3, sizeof(cl_double3), &shadows);
+        clSetKernelArg(imageKernel, 4, sizeof(cl_double3), &gamma);
+        clSetKernelArg(imageKernel, 5, sizeof(cl_double3), &lights);
+     
         // start kernel!!
-        err = clEnqueueNDRangeKernel(cmd_queue, imageKernel, 2, NULL, &globalWorkSize, NULL, 0, NULL, NULL);
+        err = clEnqueueNDRangeKernel(cmd_queue, imageKernel, 1, NULL, &globalWorkSize, NULL, 0, NULL, NULL);
         if (err != CL_SUCCESS) {
-            NSLog(@"could not run kernel");
+            NSLog(@"could not run kernel: %i", err);
         }
         
         // read output image
-        err = clEnqueueReadImage(cmd_queue, outputData, CL_TRUE, origin, region, bytesPerRow, 0, base, 0, NULL, NULL);
+        
+        err = clEnqueueReadImage(cmd_queue, outputData, CL_TRUE, origin, region, 0, 0, base, 0, NULL, NULL);
         if (err != CL_SUCCESS) {
-            NSLog(@"could not read results");
+            NSLog(@"could not read results: %i", err);
         }
         
         clReleaseKernel(imageKernel);
--- Video Filter Processor/filterKernel.cl
+++ Video Filter Processor/filterKernel.cl
@@ -1,7 +1,47 @@
 
-__kernel void imageVideoFilter(read_only image2d_t input, write_only image2d_t output,   float4  shadows, float4 mids, float4 highlights){
-    int     x = get_global_id(0);
-    int     y = get_global_id(1);
+__kernel void imageVideoFilter(read_only image2d_t input, write_only image2d_t output, sampler_t sampler, float4  shadows, float4 mids, float4 highlights){
+    int     id = get_global_id(0);
+    int id2 = get_global_id(1);
+    
+    int width = get_image_width(input);
+    int height = get_image_height(input);
+    int y = id / width;
+    int x = id % width;
+    int2 pos = (int2)(x, y);
+
+
+    
+   
+       
+    // works on cpu
+    //float4 pixel = read_imagef(input, sampler, pos);
+    uint4 uipixel = read_imageui(input, sampler, pos);
+    
+    
+    float4 pixel = convert_float4(uipixel);
+    
+    pixel = pixel / 255.0f;
+    
+    pixel = pixel * (highlights + 1.0f); // highlights
+    pixel = pixel + shadows; // shadows
+    
+    pixel = clamp(pixel, 0.0f, 1.0f);
+    
+    //pixel = pow(pixel, (1.0f - mids)); // gamma die orginal pow() funktion ist zu kostspielig, powr() und half_powr() sind nur 1 FPS schneller
+
+    
+    // WTF ?! das gibt auf der gpu GANZ andere werte!
+    pixel = native_powr(pixel, (1.0f - mids)); // gamma
+    
+    pixel = pixel * 255.0f;
+    
+    // ignore computations on the alpha channel
+    pixel.x = 255.0f;
+    
+    
+    uipixel = convert_uint4(pixel);
+    write_imageui(output, pos, uipixel);
+    //write_imagef(output, pos, pixel);
 }
 
 __kernel void bufferVideoFilter(__global unsigned char*  input, __global unsigned char * output,  float4  shadows, float4 mids, float4 highlights){
@@ -18,12 +58,10 @@ __kernel void bufferVideoFilter(__global unsigned char*  input, __global unsigne
     float4 localMids = 1.0f - mids;
     pixel = native_powr(pixel, localMids); // gamma
     pixel = pixel * 255.0f;
-     
-
-    
 
     output[x] = 255; // irgnore alpha chanel
     output[x+1] = pixel.y;
     output[x+2] = pixel.z;
     output[x+3] = pixel.w;
 }
+
