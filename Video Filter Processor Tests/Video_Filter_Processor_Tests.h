8826c2f738491900151e7306f78c369e7ad40d7a
Franziska Neumeister <info@franziska-neumeister.de>
2013-05-09 17:40:45 +0200

    Changes that didn't show up in xcode just a moment ago…

---
+++ Video Filter Processor Tests/Video_Filter_Processor_Tests.h
@@ -0,0 +1,13 @@
+//
+//  Video_Filter_Processor_Tests.h
+//  Video Filter Processor Tests
+//
+//  Created by Franzi on 26.04.13.
+//
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Video_Filter_Processor_Tests : SenTestCase
+
+@end
---
+++ Video Filter Processor Tests/Video_Filter_Processor_Tests.m
@@ -0,0 +1,32 @@
+//
+//  Video_Filter_Processor_Tests.m
+//  Video Filter Processor Tests
+//
+//  Created by Franzi on 26.04.13.
+//
+//
+
+#import "Video_Filter_Processor_Tests.h"
+
+@implementation Video_Filter_Processor_Tests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Video Filter Processor Tests");
+}
+
+@end
